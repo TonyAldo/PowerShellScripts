08677093a7d234a749c4993a261d7500041c5d47
TonyAldo <anthonyaldorasi@gmail.com>
2016-02-22 22:58:48 -0500

    Added files via upload

---
+++ Maintence.ps1
@@ -0,0 +1,12 @@
+﻿$ComputerName = $env:COMPUTERNAME
+$TempFiles = $env:TEMP
+$username = gc env:username
+$Userprofile = gc env:userprofile
+$DownloadsPath = "$Userprofile" + "\Downloads\*.*"
+$WindowsTemp = 'C:\Windows\Temp\*.*'
+
+Write-Host Your computer name is $ComputerName
+Write-Host Your user name is: $username
+
+rm $WindowsTemp
+rm $DownloadsPath
